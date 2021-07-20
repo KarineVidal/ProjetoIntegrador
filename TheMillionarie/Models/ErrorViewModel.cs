2a007d558bcb04331c60f15212df1fa64e7feee3
KarineVidal <karinevidal9@gmail.com>
2021-07-20 20:24:59 -0300

    Projeto Criado

---
+++ TheMillionarie/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheMillionarie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
